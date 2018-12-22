846ce5cd0863537a171d241e4fe3f47e44797349
polyeffect <polyeffect@gmail.com>
2018-12-22 21:39:44 +0900

    Add files via upload

--- UDPSocket.cs
+++ UDPSocket.cs
@@ -17,6 +17,10 @@ public class UDPSocket : MonoBehaviour
 
     UdpClient udpReceive; // 6000
     UdpClient udpSend; // 8888
+    static readonly object lockObject = new object();
+
+    string receiveData = "welcome";
+    string msg = "hello";
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +28,7 @@ public class UDPSocket : MonoBehaviour
         udpSend = new UdpClient(sendPort);
 
         thread = new Thread(new ThreadStart(ThreadMethod));
+        thread.Start();
     }
 
     // Update is called once per frame
@@ -32,8 +37,39 @@ public class UDPSocket : MonoBehaviour
         
     }
 
+    // ---------- receive data ----------
     private void ThreadMethod()
     {
         udpReceive = new UdpClient(receivePort);
+
+        while(true)
+        {
+            IPEndPoint RemoteIPEndPoint = new IPEndPoint(IPAddress.Any, receivePort);
+            print(RemoteIPEndPoint);
+
+            byte[] receiveBytes = udpReceive.Receive(ref RemoteIPEndPoint);
+
+            lock (lockObject)
+            {
+                receiveData = Encoding.ASCII.GetString(receiveBytes);
+                print(receiveData);
+            }
+        }
+    }
+
+    // ---------- receive data ----------
+    public void SendSocketData()
+    {
+        IPEndPoint TargetIPEndPoint = new IPEndPoint(IPAddress.Broadcast, sendPort);
+        byte[] sendBytes = Encoding.UTF8.GetBytes(msg);
+        udpSend.Send(sendBytes, sendBytes.Length, TargetIPEndPoint);
+    }
+
+    // ---------- quit applucation ----------
+    private void OnApplicationQuit()
+    {
+        udpReceive.Close();
+        udpSend.Close();
+        thread.Abort();
     }
 }
